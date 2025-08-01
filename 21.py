093a031fd5ff6c0df2cb9e1f138caf722d3958e7
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 22:29:23 -0300

    Create 21.py

---
+++ 21.py
@@ -0,0 +1,7 @@
+Faça um vetor de tamanho 70 preenchido com o seguinte valor: (i + 5*i) % (i + 1) , sendo i a posição do elemento no vetor. Em seguida imprima o vetor na tela.
+
+vetor=[]
+for i in range(70):
+    valor=(i+5*i)%(i+1)
+    vetor.append(valor)
+print(vetor)
