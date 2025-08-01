7698d8fc9fbb2e7267f95c0c8a3d5d595a19cb91
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 22:15:00 -0300

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,8 @@
+rie um programa que lê 6 valores inteiros e, em seguida, mostre na tela os valores lidos.
+
+lista=[]
+for i in range(6):
+    num=int(input())
+    lista.append(num)
+for j in lista:
+    print(j)
